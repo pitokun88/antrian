dfc95fbbb3dc78638253c47227e6f8e525b44480
pitoo-kun <mahendraalfito15@gmail.com>
2025-12-04 19:28:15 +0700

    mengedit file firebase

--- prototype/frontend/src/config/firebase-config.js
+++ prototype/frontend/src/config/firebase-config.js
@@ -1,3 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+
 // GANTI DENGAN CONFIG FIREBASE PROJECT KAMU
 const firebaseConfig = {
     apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
@@ -8,4 +13,7 @@ const firebaseConfig = {
     appId: "1:123456789:web:abcdef123456"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
